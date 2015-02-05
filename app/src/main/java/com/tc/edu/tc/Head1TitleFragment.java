fae4304c66536c2b0ef9faccc37d54b01c1c1c38
ZhaoJietong <zhaojietong@sohu.com>
2015-02-06 00:36:10 +0800

    布局-Head

---
+++ app/src/main/java/com/tc/edu/tc/Head1TitleFragment.java
@@ -0,0 +1,38 @@
+package com.tc.edu.tc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Head1TitleFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.head1_title_fragment, container, false);
+		return view;
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		System.out.println("ExampleFragment--onPause");
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		System.out.println("ExampleFragment--onResume");
+	}
+
+	@Override
+	public void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		System.out.println("ExampleFragment--onStop");
+	}
+}
