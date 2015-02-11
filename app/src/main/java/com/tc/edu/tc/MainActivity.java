21fff1f0bed50f892d1ef1a7d8395355ce1a868f
ZhaoJietong <zhaojietong@sohu.com>
2015-02-12 00:04:00 +0800

    *styles.xml *fragment > fragment中的根layout及时设定了ID也会被绘制时替换掉的，我的理解这个根layout不在被最终的界面上，而只是定义了fragment中的布局

---
+++ app/src/main/java/com/tc/edu/tc/MainActivity.java
@@ -0,0 +1,195 @@
+package com.tc.edu.tc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import de.greenrobot.event.EventBus;
+
+
+public class MainActivity extends Activity implements GestureDetector.OnGestureListener {
+    GestureDetector detector;
+    private ViewFlipper viewFlipper = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        detector = new GestureDetector(this, this);
+
+        TextView editText1 = (TextView) findViewById(R.id.editText1);
+        for (int i = 0; i < 100; i++) {
+            editText1.setText(editText1.getText() + "\n2342134");
+        }
+
+        editText1.setX(170);
+
+
+
+        //EventBus.getDefault().register(this);
+    }
+
+
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        // 触碰时间按下时触发该方法
+        Toast.makeText(this, "OnDown", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        // 当用户在屏幕上长按时触发该方法
+        Toast.makeText(this, "onLongPress", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        // 当屏幕“滚动”时触发该方法
+        Toast.makeText(this, "onScroll", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                           float velocityY) {
+        // 当用户在屏幕上“拖动”时触发该方法
+        Toast.makeText(this, "onFling", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        // 当用户在触摸屏幕上按下、而且还未移动和松开时触发该方法
+        Toast.makeText(this, "onShowPress", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        // 在屏幕上的轻击事件将会触发该方法
+        Toast.makeText(this, "onSingleTapUp", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    public void onEvent(MyEvent myEvent){
+        Log.i("xxx","onEvent");
+        switch (myEvent.getEvent()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+
+                viewFlipper.setInAnimation(this, R.anim.in_leftright);
+                viewFlipper.showNext();
+                break;
+        }
+    }
+
+    public void onEventMainThread(MyEvent myEvent){
+        Log.i("xxx","onEventMainThread");
+        switch (myEvent.getEvent()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+
+                viewFlipper.setInAnimation(this, R.anim.in_leftright);
+                viewFlipper.showNext();
+                break;
+        }
+    }
+
+
+public void onEventBackgroundThread(MyEvent myEvent){
+    Log.i("xxx","onEventBackgroundThread");
+    switch (myEvent.getEvent()) {
+        case MotionEvent.ACTION_DOWN:
+            break;
+        case MotionEvent.ACTION_UP:
+
+            viewFlipper.setInAnimation(this, R.anim.in_leftright);
+            viewFlipper.showNext();
+            break;
+    }
+}
+
+    public void onEventAsync(MyEvent myEvent){
+    Log.i("xxx","onEventAsync");
+        switch (myEvent.getEvent()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+
+                viewFlipper.setInAnimation(this, R.anim.in_leftright);
+                viewFlipper.showNext();
+                break;
+        }
+    }
+
+    public void onEvent(Integer integer){
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        EventBus.getDefault().post(new MyEvent(event.getAction()));
+        /*
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+
+                viewFlipper.setInAnimation(this, R.anim.in_leftright);
+                viewFlipper.showNext();
+                break;
+        }
+        */
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+        Handle action bar item clicks here. The action bar will
+		automatically handle clicks on the Home/Up button, so long
+		as you specify a parent activity in AndroidManifest.xml.
+		*/
+
+        if (item.getItemId() == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
+class MyEvent{
+    public int event;
+
+    MyEvent(int event) {
+        this.event = event;
+    }
+
+    public int getEvent() {
+        return event;
+    }
+}
